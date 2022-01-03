0dd86f623e7032a4fa1244907e129ee3b084f9d6
Axel Tetzlaff <axel.tetzlaff@shopify.com>
2022-01-03 15:49:11 -0500

    db based auth

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use serde::{Serialize, Deserialize};
-use warp::Filter;
+use sqlx::{Pool, Postgres, PgPool, PgExecutor};
+use warp::log::Log;
+use warp::{Filter, Rejection, Reply};
 use warp::reply::Json;
+use sqlx::postgres::PgPoolOptions;
 
 #[derive(Serialize, Deserialize)]
 struct Login {
@@ -14,49 +17,59 @@ enum LoginResponse {
     Failure(String),
 }
 
+fn with_db(pool: PgPool) -> impl Filter<Extract = (PgPool,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || pool.clone())
+}
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://rust@localhost/rust_server").await.unwrap();
 
     let login = warp::path("login")
         .and(warp::body::content_length_limit(1024 * 16))
+        .and(with_db(pool))
         .and(warp::body::json())
-        .map(|login: Login| auth(&login));
+        .and_then(move |local_pool: PgPool, login: Login| auth_handler(local_pool, login));
 
-    let get_routes = warp::get().and(hello);
     let post_routes = warp::post().and(login);
 
 
-    warp::serve(get_routes.or(post_routes))
+    warp::serve(post_routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
 
-fn auth(login: &Login) -> Json {
-    let login_response =
-        match find_user_by_username(&login.username, &login.password) {
-            Some(user) => LoginResponse::Success(user.username),
-            None => LoginResponse::Failure("not found".to_string()),
+
+async fn auth_handler(ext_pool: PgPool, login: Login) -> std::result::Result<impl Reply, Rejection> {
+    let user = sqlx::query_as::<_, User>("SELECT username, hashed_password FROM users WHERE username = $1")
+        .bind(login.username)
+        .fetch_one(&ext_pool).await;
+
+
+    let login_response = 
+        match user {
+            Ok(user) => if user.hashed_password.eq(&login.password) { LoginResponse::Success(user.username) } else { LoginResponse::Failure("wrong pw".to_string()) },
+            Err(_) => LoginResponse::Failure("not found".to_string()),
         };
-    warp::reply::json(&login_response)
+    Ok(warp::reply::json(&login_response))
 }
 
-#[derive(Clone)]
+#[derive(Clone, sqlx::FromRow)]
 struct User {
     username: String,
     hashed_password: String,
 }
 
-fn find_user_by_username(username: &String, password: &String) -> Option<User> {
+fn find_user_by_username_and_password(username: &String, password: &String) -> Option<User> {
     let users:Vec<User> =
      [ User { username: "at".to_string(), hashed_password: "aa".to_string() }
      , User { username: "rb".to_string(), hashed_password: "rb".to_string() } ].to_vec();
 
-    match users.iter().find(|u| u.username.eq(username)) {
+    let res = match users.iter().find(|u| u.username.eq(username)) {
        Some(user) => if user.hashed_password.eq(password) { Some(user.clone()) } else { None },
        None => None
-    }
+    };
+    res
 }
