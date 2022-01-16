b4effaa96ad08d4ee8558e889f938f31a9b3cc4b
Axel Tetzlaff <axel.tetzlaff@shopify.com>
2022-01-16 17:22:18 -0500

    Collect users in minimal example

--- minimal/src/main.rs
+++ minimal/src/main.rs
@@ -1,12 +1,27 @@
+use std::sync::{Arc, RwLock};
 use warp::Filter;
+
+#[derive(Clone)]
+struct Env {
+    users: Arc<RwLock<Vec<String>>>,
+}
 
 #[tokio::main]
 async fn main() {
+    let env = Env {
+        users: Arc::new(RwLock::new(Vec::new())),
+    };
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+        .and(with_env(env.clone()))
+        .map(|name: String, env: Env| {
+            env.users.write().unwrap().push(name.clone());
+            format!("Hello, {}!", name)
+        });
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+}
+
+fn with_env(env: Env) -> impl Filter<Extract = (Env,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || env.clone())
 }
